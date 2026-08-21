c6213883077ebaad7513409c2eb44bfe0d868812
Tushar Molla <tusharmolla.csm@gmail.com>
2026-08-21 18:00:24 +0600

    IProductCatalogClient (Step2)

---
+++ OrderService/OrderService.Application/Interfaces/IProductCatalogClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderService.Application.Interfaces
+{
+    public interface IProductCatalogClient
+    {
+        Task<IEnumerable<ProductCatalogInfo>> GetProductsByIdsAsync(IEnumerable<Guid> productIds);
+    }
+    public class ProductCatalogInfo
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+    //Step 2
+}
