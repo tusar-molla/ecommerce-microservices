8fa87d5a0d7370ac33c3be77cb3a7f02d1d80878
Tushar Molla <tusharmolla.csm@gmail.com>
2026-09-23 17:43:59 +0600

    Add ECommerce.Contracts shared library with OrderPlacedEvent definition

---
+++ ECommerce.Contracts/Events/OrderPlacedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class OrderPlacedEvent
+{
+    public Guid OrderId { get; set; }
+    public Guid UserId { get; set; }
+    public List<OrderPlacedItem> Items { get; set; } = new();
+    public DateTime OccurredAt { get; set; } = DateTime.UtcNow;
+}
+
+public class OrderPlacedItem
+{
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+}
